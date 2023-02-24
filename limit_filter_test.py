17c74693a04df40976a5e7461475a84d6783d797
LoreLHR <125083061+LoreLHR@users.noreply.github.com>
2023-02-24 08:58:45 +0100

    Update limit_filter_test.py

--- limit_filter_test.py
+++ limit_filter_test.py
@@ -60,26 +60,6 @@ def st_audio(A, samplerate=44100):
     st.audio(byte_io)
 
     
-t=arange(-30,30,0.2)
-fig,ax = subplots(figsize=(15,5))
-st.title('Choose limit, test algorithm')
-st.subheader('Type')
-ch=st.radio('Choose filter type', ['Low pass', 'Band pass','Hight pass'])
-st.subheader('Parameter')
-if (ch=='Band pass'):
-    w1=st.slider('Choose first frequency', 0, 10000)
-    w2=st.slider('Choose second frequency', 0, 10000)
-    st.subheader('Result')
-    s1=w1*np.ones(t.size)
-    s2=w2*np.ones(t.size) 
-    plot(s1,t)
-    plot(s2,t)
-    st.pyplot(fig,ax)
-else :     
-    w=st.slider('Choose frequency', 0, 10000)
-    st.subheader('Result')
-    s=w*np.ones(t.size) 
-    plot(s,t)
-    st.pyplot(fig,ax)
+
 
     
